ddca1babaa73e065c3d23856a3b820ac0ef85e69
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-03-27 20:27:02 -0500

    Function overloading

--- person.cpp
+++ person.cpp
@@ -5,6 +5,8 @@ using namespace std;
 class person {
 	
 	private:
+	
+		string name;
 		string gender;
 		int age;
 		string race;
@@ -13,15 +15,17 @@ class person {
 	public:
 	
 		Person(){
-			name = "unknown";
-			gender = "M";
-			race = "ugly";
+			setName("unknown");
+			setGender("M");
+			setRace("ugly");
+			setAge(age);
 		}
 		
 		person(string n, string g, string r){
-			name = n;
-			gender = g;
-			race = r;
+			setName(n);
+			setGender(g);
+			setRace(r);
+			setAge(age);
 		}
 	
 		string getStats(){
@@ -35,8 +39,15 @@ class person {
 		bool getAlive(){ return is_alive; }
 	
 		void setName(string n){name = n; }
-		void setGender(string g){gender = g; }		
-		void setAge(int a){age = a; }
-		void setRace(string r){race = r; }
-		void setAlive(bool a){is_alive = a; }
+		void setGender(string g){gender = g; }	
+		void setRace(string r { race = r;}
+		void setAlive(bool a){is_alive = a;}
+		void setAge(int a){
+				if( (0 > a) || (150 > a) ){
+				age = 25;
+				} else {
+				age = a;
+				}
+		}
+		
 };
